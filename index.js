f81da90374d37d40c3438788ad02685d3ceebc0c
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-08 21:09:19 +0300

    new iterations and percentage of success

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const brain = require('brain.js');
-const fs = require('fs');
-const model = JSON.parse(fs.readFileSync('./model.json', 'utf8')); // Import the model
+const brain = require("brain.js");
+const fs = require("fs");
+const model = JSON.parse(fs.readFileSync("./model.json", "utf8")); // Import the model
+const kufur = require("./kufur.json");
 
 const net = new brain.recurrent.LSTM();
 net.fromJSON(model); // Include the model
+let unCaught = 0;
 
-const output = net.run('guzel'); // Run
+kufur.forEach(({input}) => {
 
-console.log(output);
+    const output = net.run(input); // Run
+
+    console.log("kufur: ", input, " - sonuc: ", output);
+    if (output == 0) unCaught++;
+});
+
+const totalSuccessPercentage = ((kufur.length - unCaught) * 100) / kufur.length;
+
+console.log(unCaught, " adet kufur algilanamadi");
+console.log(totalSuccessPercentage, "% oraninda basari saglandi");
--- train.js
+++ train.js
@@ -8,20 +8,19 @@ const net = new brain.recurrent.LSTM();
  * Train the model with local data
  */
 
-const TRAINING_DATA = data.map(item => ({
-    input: item.input,
-    output: item.output,
+const TRAINING_DATA = data.map(({input, output}) => ({
+    input,
+    output,
 }));
-
-// const TRAINING_DATA = [...nonProfanityData, ...profanityData]; // Merge profanity and non-profanity data
 
 // Training options, more options be able to added
 const options = {
-    iterations: 5000,
+    iterations: 24000,
 };
 
+console.log("Training started...");
 net.train(TRAINING_DATA, options); // Training
 const model = net.toJSON(); // Create a model
 
 
-fs.writeFile('./model.json', JSON.stringify(model), 'utf8', () => console.log('model has been written')); // Export the model
+fs.writeFile('./model.json', JSON.stringify(model), 'utf8', () => console.log("Model has been written \n Training ended...")); // Export the model
